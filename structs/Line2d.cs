38932e64acccd46e79543d8d7c0edeaa42462906
Zdeněk Borovec <zdenda.borovec@gmail.com>
2024-03-05 18:03:22 +0100

    Added Lines

---
+++ structs/Line2d.cs
@@ -0,0 +1,29 @@
+﻿namespace GeometriCS.structs
+{
+    /// <summary>
+    /// A line through a two-dimensional vector space.
+    /// </summary>
+    public class Line2d
+    {
+        /// <summary>
+        /// A point that lies on the line.
+        /// </summary>
+        public Vector2d PointOnLine { get; set; }
+
+        /// <summary>
+        /// Direction vector, along which all the points on the line lie.
+        /// </summary>
+        public Vector2d Direction { get; set; }
+
+        /// <summary>
+        /// Constructor for the line.
+        /// </summary>
+        /// <param name="pointOnLine">Point that lies on the line.</param>
+        /// <param name="direction">Direction vector on the line.</param>
+        public Line2d(Vector2d pointOnLine, Vector2d direction)
+        {
+            PointOnLine = pointOnLine;
+            Direction = direction;
+        }
+    }
+}
---
+++ structs/Line3d.cs
@@ -0,0 +1,29 @@
+﻿namespace GeometriCS.structs
+{
+    /// <summary>
+    /// A line through the R cubed space with double precision.
+    /// </summary>
+    public class Line3d
+    {
+        /// <summary>
+        /// A point that lies on the line.
+        /// </summary>
+        public Vector3d PointOnLine { get; set; }
+
+        /// <summary>
+        /// Direction vector, along which all the points on the line lie.
+        /// </summary>
+        public Vector3d Direction { get; set; }
+
+        /// <summary>
+        /// Constructor for the line.
+        /// </summary>
+        /// <param name="pointOnLine">Point that lies on the line.</param>
+        /// <param name="direction">Direction vector on the line.</param>
+        public Line3d(Vector3d pointOnLine, Vector3d direction) 
+        {
+            PointOnLine = pointOnLine;
+            Direction = direction;
+        }
+    }
+}
